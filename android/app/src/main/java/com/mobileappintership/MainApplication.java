6509e512a4a2f34b2aa3a6edbee4a9773cec646f
Samir Romdhani <romdhanisamir94@gmail.com>
2017-12-10 12:50:44 +0000

    commit2

---
+++ android/app/src/main/java/com/mobileappintership/MainApplication.java
@@ -0,0 +1,65 @@
+package com.mobileappintership;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import com.evollu.react.fcm.FIRMessagingPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new ReactNativeContacts(),
+            
+       
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new RNFetchBlobPackage(),
+            new SplashScreenReactPackage(),
+            new ReactNativePushNotificationPackage(),
+            new ReactNativeConfigPackage(),
+            new ReactNativeI18n(),
+            new VectorIconsPackage(),
+            new FIRMessagingPackage(),
+            new RNDeviceInfo()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
